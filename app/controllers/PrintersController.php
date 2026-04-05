27e293b70d9adcaab6c4622ec6b1461d102896e6
edson <edsonjmv@outlook.com>
2026-04-04 20:54:41 -0600

    test-print endpoint

--- app/controllers/PrintersController.php
+++ app/controllers/PrintersController.php
@@ -82,6 +82,64 @@ class PrintersController extends Controller
         ]);
     }
 
+    public function testPrint()
+    {
+        /* Validating */
+        $validatedData = request()->validate([
+            'header' => 'array<array>',
+            'articles' => 'array<array>',
+            'totals' => 'array<array>',
+            'footer' => 'array<array>',
+        ]);
+
+        try {
+            $printObject = [
+                'header' => [],
+                'articles' => [],
+                'totals' => [],
+                'footer' => []
+            ];
+            /* PRINT */
+            /* Header */
+            foreach ($validatedData['header'] as $item) {
+                $printObject['header'][] = $item;
+            }
+
+            /* Articles */
+            foreach ($validatedData['articles'] as $item) {
+                $qty = $item['quantity'] ?? '';
+                $name = trim(strlen($item['name']) > 20 ? substr($item['name'] ?? '', 0, 20) : $item['name'] ?? ''); // Clipped product name (max 20 chars)
+                $unitPrice = $item['unitPrice'] ?? '';
+                $total = $item['total'] ?? '';
+
+                // Format: "qty  product_name  unit_price  total"
+                $line = sprintf("%-4s %-20s %8s %10s", $qty, $name, $unitPrice, $total);
+                $printObject['articles'][] = $line;
+            }
+
+            /* Totals */
+            foreach ($validatedData['totals'] as $item) {
+                $printObject['totals'][] = $item;
+            }
+
+            /* Footer */
+            foreach ($validatedData['footer'] as $item) {
+                $printObject['footer'][] = $item;
+            }
+
+            /* Close */
+        } catch (Exception $e) {
+            response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
+
+        response()->json([
+            'message' => 'Listo!',
+            'data' => $printObject
+        ]);
+    }
+
     public function getPrinterName() {
         response()->json([
             'printerName' => _env('PRINTER_NAME', 'printer')
--- app/routes/_print.php
+++ app/routes/_print.php
@@ -1,4 +1,5 @@
 <?php
 
 app()->post('/print', 'PrintersController@print');
+app()->post('/test-print', 'PrintersController@testPrint');
 app()->get('/printer-name', 'PrintersController@getPrinterName');
